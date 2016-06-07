31fa0204c27456bff474e3ce5b915154e300008e
Doron Grinzaig <doron@jifiti.com>
2016-06-07 21:51:53 +0300

    first commit

---
+++ Template/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Template.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+        public ActionResult ProductPage()
+        {
+
+            return View();
+        }
+
+        public ActionResult CategoryFeed()
+        {
+            return View();
+        }
+
+        public ActionResult UploadStory()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            return View();
+        }
+    }
+}
